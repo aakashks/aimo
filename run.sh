40db7f232f14f5d3267c9aacb5fad27731910bcd
aks-online-v <114357454+aakashks@users.noreply.github.com>
2024-06-11 23:08:42 +0530

    prompt and modulo changes

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name s1
+#SBATCH --job-name=3n
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=6:00:00
+#SBATCH --time=8:00:00
 
 source ~/.bashrc
 conda activate temp
 z aimo
 
-python aimo3.py
+FILE="aimo3n.py"
+
+cat $FILE
+python $FILE
+python $FILE
