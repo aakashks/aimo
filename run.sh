4af8977dd0eb3c094b6c4d348757a35e21266b9c
aks-online-v <114357454+aakashks@users.noreply.github.com>
2024-06-14 02:43:24 +0530

    add

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=t
+#SBATCH --job-name=3b
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks-per-node=20
 #SBATCH --time=8:00:00
+#SBATCH --output=%x-%j.out
 
 source ~/.bashrc
 z aimo
 
-FILE="$1"
-
-cat $FILE
-python $FILE
-python $FILE
+cat run.sh
+cat aimo3b.py
+python aimo3b.py --cfile code21.py --temp 0.8
+python aimo3b.py --cfile code22.py --temp 0.8
